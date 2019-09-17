26da2d1736c06907a68b411850e9577f4cb3ca67
Anastasia Pankina <siapankina@gmail.com>
2019-09-17 14:07:58 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,9 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name=>"Lord Montague",
+        :age=>"53"
         #your key/value pairs here
         },
       :matriarch => {
+        :name=> "Lady"
         #your key/value pairs here
         },
       :hero => {
