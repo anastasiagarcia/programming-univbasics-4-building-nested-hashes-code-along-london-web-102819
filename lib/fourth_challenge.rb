034333d4befddcf183ee5f5789ceed96404c5921
Anastasia Pankina <siapankina@gmail.com>
2019-09-17 14:19:15 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,6 @@ def fourth_challenge
         :age=> "18",
         :attitude=> "hot-headed"
         }
-      ]
         #your hashes here!
       ]
    }, 
@@ -24,6 +23,17 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        {
+        :name=> "Steven",
+        :age=>"30"
+        :attitude=> "confused"
+        },
+        {
+        :name=> "Nurse",
+        :age=> "44",
+        :attitude=> "worried"
+        }
+  
         #your hashes here!
       ]
    }
